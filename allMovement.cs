5f53794f1d81697bf234e8b36de8d128b499f889
GiuseppeCaiolo <80903861+GiuseppeCaiolo@users.noreply.github.com>
2021-03-30 09:27:15 +0200

    Add files via upload

---
+++ allMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class allMovement : MonoBehaviour
+{
+	public CharacterController player;
+	private float speed = 20f;
+	private float moveX,moveZ;
+	private Vector3 move;
+	
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        moveZ = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        move = transform.right * moveX + transform.forward * moveZ;
+        player.Move(move);
+    }
+}
